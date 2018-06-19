b67366240918e3d45cee2c73cec2948544addcc4
Rodrigo Flores <rflor012@gmail.com>
2018-06-19 15:15:19 -0400

    V1 done, need to attach prices to Labels, read up

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,39 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+
+  $(".pep").hide();
+  $(".mushroom").hide();
+  $(".green-pepper").hide();
+  $(".sauce").removeClass("sauce-white");
+  $(".crust").removeClass("crust-gluten-free");
+  $(".btn").removeClass("active");
+  $("li:contains(w)").hide();
+  $("li:contains(gl)").hide();
+
+
+
+  $(".btn-pepperonni").click(function(){
+    $(".pep").toggle();
+    $(".btn-pepperonni").toggleClass("active");
+  });
+
+  $(".btn-mushrooms").click(function(){
+    $(".mushroom").toggle();
+    $(".btn-mushrooms").toggleClass("active");
+  });
+
+  $(".btn-green-peppers").click(function(){
+    $('.green-pepper').toggle();
+    $(".btn-green-peppers").toggleClass("active");
+  });
+
+  $(".btn-sauce").click(function(){
+    $(".sauce").toggleClass("sauce-white");
+    $(".btn-sauce").toggleClass("active");
+  });
+
+  $('.btn-crust').click(function(){
+    $(".crust").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
+  });
+
+});
